7cfaccb77635c84cdead2908998bc553ef29ea43
imperiummotors <benedictpartnership@gmail.com>
2026-05-17 14:39:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const tl = gsap.timeline();
 
+tl.to("#shield-logo",{
+
+    opacity:1,
+    duration:2,
+    scale:1.1
+
+})
+
+.to("#shield-logo",{
+
+    rotation:360,
+    duration:2
+
+})
+
+.to("#imperium-title",{
+
+    opacity:1,
+    y:-10,
+    duration:1.5
+
+})
+
+.to("#legacy-text",{
+
+    opacity:1,
+    duration:2
+
+});
